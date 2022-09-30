3e39898cf9aed67d81dba121b7ede0d378391b53
vishwadeaero <vishwadeaero@gmail.com>
2022-09-30 19:40:46 +0530

    movie cards section added

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <!-- Custom styles -->
     <link href="assets/css/main.css" rel="stylesheet">
     <style>
-    
+
     </style>
     <title>Kingsman Tickets</title>
 </head>
@@ -53,7 +53,7 @@
     </nav>
 
     <!-- Main Slider -->
-    <div id="carouselExampleCaptions" class="carousel slide main-slider" data-bs-ride="carousel">
+    <div id="carouselExampleCaptions" class="carousel slide main-slider mb-lg-5" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
                 aria-current="true" aria-label="Slide 1"></button>
@@ -64,21 +64,24 @@
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="assets/images/main_slider/movies_1315_633145515b98f_naane_varu_sd.jpg" class="d-block w-100" alt="Naane Varuen">
+                <img src="assets/images/main_slider/movies_1315_633145515b98f_naane_varu_sd.jpg" class="d-block w-100"
+                    alt="Naane Varuen">
                 <div class="carousel-caption d-none d-md-block">
                     <h1>First slide label</h1>
                     <h3>Some representative placeholder content for the first slide.</h3>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="assets/images/main_slider/movies_1170_6045ea459dd42_sinam_sd.jpg" class="d-block w-100" alt="sinam">
+                <img src="assets/images/main_slider/movies_1170_6045ea459dd42_sinam_sd.jpg" class="d-block w-100"
+                    alt="sinam">
                 <div class="carousel-caption d-none d-md-block">
                     <h1>Second slide label</h1>
                     <h3>Some representative placeholder content for the second slide.</h3>
                 </div>
             </div>
             <div class="carousel-item">
-                <img src="assets/images/main_slider/movies_1301_62f6378b2572d_thiruchi_sd.jpg" class="d-block w-100" alt="thiruchan">
+                <img src="assets/images/main_slider/movies_1301_62f6378b2572d_thiruchi_sd.jpg" class="d-block w-100"
+                    alt="thiruchan">
                 <div class="carousel-caption d-none d-md-block">
                     <h1>Third slide label</h1>
                     <h3>Some representative placeholder content for the third slide.</h3>
@@ -97,6 +100,46 @@
         </button>
     </div>
     <!-- Main Slider End -->
+
+    <!-- Latest Movies Section -->
+    <div class="container-fluid p-lg-5">
+        <div class="row">
+            <div class="col">
+                <h2>Latest Movies to Watch</h2>
+            </div>
+        </div>
+        <hr class="mb-lg-4">
+        <div class="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://picsum.photos/350/400" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-auto">
+                                <h5 class="card-title">Card title</h5>
+                                <h6 class="">Category</h6>
+                            </div>
+                            <div class="col text-end align-items-center d-flex justify-content-end">
+                                <small class="bg-warning p-2 rounded-3 align-middle">
+                                    Rating:
+                                    <span>
+                                        4.8/5</span>
+                                </small>
+                            </div>
+                        </div>
+                        <hr>
+                        <p class="card-text"><small class="text-muted">This is a longer card with supporting text below
+                                as a natural lead-in to
+                                additional content. This content is a little bit longer.</small></p>
+                        <div class="d-grid gap-2">
+                            <button type="button" class="btn btn-lg btn-dark text-light">Buy Tickets</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Latest Movies Section End-->
 
     <!-- Javascript -->
     <script src="bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
