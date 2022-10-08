76378d0d6cb02313af348a55608b4f2221f4e909
vishwadeaero <vishwadeaero@gmail.com>
2022-10-08 10:17:55 +0530

    header footer and links seperated

--- index.php
+++ index.php
@@ -2,56 +2,12 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Bootstrap -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <!-- Custom styles -->
-    <link href="assets/css/main.css" rel="stylesheet">
-    <style>
-
-    </style>
+    <?php include('master/headlinks.php') ?>
     <title>Kingsman Tickets</title>
 </head>
 
 <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light sticky-top navbar-yellow">
-        <div class="container-fluid">
-            <!-- Brand Name and Logo -->
-            <a href="#" class="navbar-brand"><strong>Kingsman Tickets</strong></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">News</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">My Account</a>
-                    </li>
-                </ul>
-                <div class="d-flex">
-                    <a href="#" class="btn btn-outline-dark">Sign In</a>
-                    <a href="#" class="ms-2 btn btn-outline-dark">Register</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-
+    <?php include('master/header.php'); ?>
     <!-- Main Slider -->
     <div id="carouselExampleCaptions" class="carousel slide main-slider mb-lg-5" data-bs-ride="carousel">
         <div class="carousel-indicators">
@@ -141,8 +97,10 @@
     </div>
     <!-- Latest Movies Section End-->
 
+    <?php include('master/footer.php'); ?>
+
     <!-- Javascript -->
-    <script src="bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <?php include('master/jslinks.php'); ?>
 </body>
 
 </html>
---
+++ master/headlinks.php
@@ -0,0 +1,12 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Bootstrap -->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<!-- Font Awesome 6 -->
+<link href="assets/libraries/fontawesome/css/fontawesome.css" rel="stylesheet">
+<link href="assets/libraries/fontawesome/css/brands.css" rel="stylesheet">
+<link href="assets/libraries/fontawesome/css/solid.css" rel="stylesheet">
+<!-- Custom styles -->
+<link href="assets/css/main.css" rel="stylesheet">
