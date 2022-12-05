20d08dc48841f7ed0ac678b0753a1ef3896ffd10
vishwadeaero <vishwadeaero@gmail.com>
2022-12-05 20:04:03 +0530

    movies show

--- controllers/allmovies.php
+++ controllers/allmovies.php
@@ -12,26 +12,33 @@
     if( !isset($Result['error']) ) {
 
         switch($_POST['function']) {
+            
+            case 'allmovies':
+                $categoryList = getAllCategories();
+                $categories = [];
+                foreach ($categoryList as $key => $value) {
+                    $cat_info = [];
+                    $cat_info['id'] = $value['id'];
+                    $cat_info['name'] = $value['name'];
+                    $cat_info['movies'] = getMovies( $value['id']);
+                    array_push($categories, $cat_info);
+                }
+                $Result['status'] = 200;
+                $Result['result'] = $categories;
+                break;
+
             case 'listcategories':
                 $categoryList = getAllCategories();
                 $Result['status'] = 200;
                 $Result['result'] = $categoryList;
                 break;
+
             case 'listmovies':
                 $category_id = $_POST['category_id'];
                 $movieList = getMovies($category_id);
                 $Result['status'] = 200;
                 $Result['result'] = $movieList;
                 break;
-            case 'list':
-                $screenList = getAllScreens();
-                $Result['status'] = 200;
-                $Result['result'] = $tableArray;
-                break;
-
-            case 'add':                
-                $name = $_POST['name'];
-               break;
 
             default:
                $Result['error'] = 'Function '.$_POST['function'].' Not Found!';
