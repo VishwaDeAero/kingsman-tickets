5a3541c2012f14f6cb18794de5cd89f926322c21
vishwadeaero <vishwadeaero@gmail.com>
2022-12-09 20:49:21 +0530

    news model added

---
+++ models/news.php
@@ -0,0 +1,121 @@
+<?php
+require_once('../env.php');
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch(PDOException $e) {
+    return "Connection failed: " . $e->getMessage();
+  }
+$conn = null;
+
+// Get Single News
+function getSingleNews($id){
+    global $servername, $dbname, $username, $password;
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = $conn->prepare("SELECT * FROM news WHERE id=$id AND deleted_at IS NULL");
+        $sql->execute();
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
+      } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+      }
+      $conn = null;
+}
+
+// Get Latest News
+function getLatestNews($records){
+    global $servername, $dbname, $username, $password;
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = $conn->prepare("SELECT * FROM news WHERE deleted_at IS NULL ORDER BY created_at LIMIT $records");
+        $sql->execute();
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
+      } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+      }
+      $conn = null;
+}
+
+// Get All News
+function getAllNews(){
+    global $servername, $dbname, $username, $password;
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = $conn->prepare("SELECT * FROM news WHERE deleted_at IS NULL");
+        $sql->execute();
+        return $sql->fetchAll(PDO::FETCH_ASSOC);
+      } catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+      }
+      $conn = null;
+}
+
+// Insert New News
+function insertNews($title, $description, $img_path){
+    global $servername, $dbname, $username, $password;
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO news (name, category_id, description, img_path)
+        VALUES ('$title', '$description', '$img_path')";
+        $conn->exec($sql);
+        $last_id = $conn->lastInsertId();
+        $return_array = [
+          "status" => 'Success',
+          "id" => $last_id
+        ];
+        return $return_array;
+      } catch(PDOException $e) {
+        return $sql . "<br>" . $e->getMessage();
+      }
+      $conn = null;
+}
+
+// Update a News
+function updateNews($id, $data){
+    global $servername, $dbname, $username, $password;
+    $date = date('Y-m-d H:i:s');
+    $updateDataQuery = "";
+    foreach($data as $key => $value) {
+        $updateDataQuery .= "$key = '$value', ";
+    }
+    $updateDataQuery = rtrim($updateDataQuery, ", ");
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = $conn->prepare("UPDATE news SET $updateDataQuery, updated_at='$date' WHERE id=$id");
+        $sql->execute();
+        return "Record: $id update successfully";
+      } catch(PDOException $e) {
+        return "Error: " . $e->getMessage();
+      }
+      $conn = null;
+}
+
+// Delete a News (Soft Delete)
+function deleteNews($id){
+    global $servername, $dbname, $username, $password;
+    $date = date('Y-m-d H:i:s');
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = $conn->prepare("UPDATE news SET deleted_at='$date' WHERE id=$id");
+        $sql->execute();
+        return "Record: $id deleted successfully";
+      } catch(PDOException $e) {
+        return "Error: " . $e->getMessage();
+      }
+      $conn = null;
+}
+?>
