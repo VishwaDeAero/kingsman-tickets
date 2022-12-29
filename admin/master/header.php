fe9fdb2e8628a7bde034c0bf91a52f0b2a68c91d
vishwadeaero <vishwadeaero@gmail.com>
2022-12-29 22:45:14 +0530

    admin logout

--- admin/master/header.php
+++ admin/master/header.php
@@ -1,7 +1,7 @@
 <?php
 // Redirect to Login If User not Logged in
 if(!isset($_SESSION["user"])){
-    header('Location: login.php');
+    header('Location: ../login.php');
     die();
 }else{
     $usertype = $_SESSION["user"]["user_type"];
@@ -10,20 +10,30 @@ if(!isset($_SESSION["user"])){
         die(); 
     }
 }
+
+// Signout Function
+if(isset($_POST['signoutBtn'])) {
+   unset ($_SESSION["user"]);
+   header('Location: ../login.php');
+}
 ?>
 
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 d-md-inline d-none" href="#">Cineflix Admin Panel</a>
-    <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse"
+        data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="navbar-nav">
         <div class="d-flex nav-item align-items-center justify-content-end">
             <div class="text-light">
-                Howdy Admin!
+                Howdy <?php echo $_SESSION["user"]["first_name"]?>!
             </div>
             <div class="text-nowrap">
-                <a class="nav-link px-3" href="#">Sign out</a>
+                <form method="POST">
+                    <button class="nav-link btn px-3" type="submit" name="signoutBtn" value="signoutBtn">Sign
+                        out</button>
+                </form>
             </div>
         </div>
     </div>
